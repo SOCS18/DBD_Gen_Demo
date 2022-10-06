f4a2d9f11e3e28f9968d722881f74ebe6493cd61
Saul Carreon <sacarreon18@gmail.com>
2022-10-06 15:18:45 -0500

    changed script

--- DBD_Gen_Demo/Assets/Scripts/GeneratorStats.cs
+++ DBD_Gen_Demo/Assets/Scripts/GeneratorStats.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GeneratorStats : MonoBehaviour
 {
     public GenRepairController genRepairController;
-    
+
     public GameObject genProgressBar;
     
     public float genRepair = 0f;
@@ -15,7 +16,7 @@ public class GeneratorStats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        genProgressBar = transform.Find("GenCanvas").GetChild(0).gameObject;
     }
 
     // Update is called once per frame
