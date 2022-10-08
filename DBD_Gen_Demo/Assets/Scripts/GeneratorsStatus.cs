fcf828cee9392a2c620d98d8eee775e850b108fe
Saul Carreon <sacarreon18@gmail.com>
2022-10-08 16:20:50 -0500

    included gen num text

--- DBD_Gen_Demo/Assets/Scripts/GeneratorsStatus.cs
+++ DBD_Gen_Demo/Assets/Scripts/GeneratorsStatus.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GeneratorsStatus : MonoBehaviour
 {
     public GameObject genParent;
     public GameObject[] generators;
     public float[] genHealth;
-    private int numGenerators;
+    public int numGenerators;
 
     // Start is called before the first frame update
     void Start()
