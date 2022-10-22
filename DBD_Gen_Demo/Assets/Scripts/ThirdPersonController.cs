9c75ce01c9a140a31dc4145250692cebda2644fc
Saul Carreon <sacarreon18@gmail.com>
2022-10-21 19:02:18 -0500

    included gravity

--- DBD_Gen_Demo/Assets/Scripts/ThirdPersonController.cs
+++ DBD_Gen_Demo/Assets/Scripts/ThirdPersonController.cs
@@ -10,9 +10,17 @@ public class ThirdPersonController : MonoBehaviour
     public Transform cam;
 
     public float speed = 6f;
+    public float gravity = -9.81f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
     
     // Start is called before the first frame update
     void Start()
@@ -24,7 +32,14 @@ public class ThirdPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
         // INCLUDE GRAVITY
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
         
         // if player is repairing generator then you can't move
         if (!genRepairController.isReparingGen)
